d51cb8db16357f955f325e3cc26477704734abaa
Katie Hoffman <ktahoffman@gmail.com>
2015-02-05 11:58:37 -0500

    update readme and add one more test

--- spec/alphabetize_spec.rb
+++ spec/alphabetize_spec.rb
@@ -2,21 +2,27 @@ describe "#alphabetize" do
 
   # ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-  it 'alphabetizes elements in an array based on the first letter' do
+  it 'alphabetizes based on the first letter' do
     expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
     sorted_expressions = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
     expect(alphabetize(expressions)).to eq(sorted_expressions)
   end
 
-  it 'alphabetizes elements in an array based on second letters' do
+  it 'alphabetizes based on second letters' do
     g_words = ["ĝuste", "ĝis revido"]
     sorted_g_words = ["ĝis revido", "ĝuste"]
     expect(alphabetize(g_words)).to eq(sorted_g_words)
   end
 
-  it 'alphabetizes elements in an array based on sixth letters' do
+  it 'alphabetizes based on sixth letters' do
     greetings = ["bonan matenon", "bonan vesperon", "bonan nokton"]
     sorted_greetings = ["bonan matenon", "bonan nokton", "bonan vesperon"]
+    expect(alphabetize(greetings)).to eq(sorted_greetings)
+  end
+
+  it 'alphabetizes based on second letters not in English alphabet' do
+    greetings = ["ŝĝĉĥŭĵ", "ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ"]
+    sorted_greetings = ["ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĵ"]
     expect(alphabetize(greetings)).to eq(sorted_greetings)
   end
 
