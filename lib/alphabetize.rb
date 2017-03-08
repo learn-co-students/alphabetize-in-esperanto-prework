65b73806a0d547dc0777ff75c5d227aae9cce110
Sammy Steiner <sammysteiner@gmail.com>
2017-03-08 20:04:21 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  ordered = {}
+  ESPERANTO_ALPHABET.split(//).each_with_index do |char, index|
+    ordered[char.to_s] = index
+  end
+  arr.sort_by do |phrase|
+    [
+    ordered[phrase[0]],
+    ordered[phrase[1]],
+    ordered[phrase[6]],
+    ordered[phrase[5]]
+    ]
+  end
 end
