76e8ff01a57fea2f736ac5536e2751d60df8d7b1
Jason Sears <jsears4585@gmail.com>
2017-03-22 23:49:07 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".codepoints
+  arr.sort_by! do |x|
+    x.codepoints.map do |char|
+      alphabet.index(char)
+      # binding.pry
+    end
+  end
 end
