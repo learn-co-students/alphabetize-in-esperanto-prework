bfb411831501a7deb36582caa68700247710445a
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-02 17:16:57 -0400

    Sorted

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word|
+    word.split("").collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
