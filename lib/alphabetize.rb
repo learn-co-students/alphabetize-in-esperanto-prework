34977042f8a7065a3d0c0556f0838e3e68ddc720
Maxwell Benton <maxwellbenton@gmail.com>
2017-04-03 21:31:57 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+#binding.pry
+arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+binding.pry
 end
