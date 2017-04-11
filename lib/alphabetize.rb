ca378df0c720e5e0ab0a74422df6de761fb08264
Luis Arita <luis@ccc-it.net>
2017-04-11 01:45:28 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+#added space to allow for phrase comparision
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort do |a, b|
+    compare_length = [a.size, b.size].min
+    for i in (0..compare_length)
+      if ESPERANTO_ALPHABET.index(a[i]) < ESPERANTO_ALPHABET.index(b[i])
+        r = -1
+      elsif ESPERANTO_ALPHABET.index(a[i]) > ESPERANTO_ALPHABET.index(b[i])
+        r = 1
+      else
+        r = 0
+      end
+      if r != 0
+        break
+      end
+    end
+    r
+  end
 end
