f4af64cded5533c4dd260b89be9f00c476702918
drzly777 <andrew.zheng725@gmail.com>
2017-04-17 17:21:10 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
+  max_word = arr.max_by{|string| string.length}
+  max_len = max_word.length
+  arr.sort_by {|string|
+    total = 0
+    str_arr = string.chars
+    str_arr.each_with_index {|val,index|
+      total += ESPERANTO_ALPHABET.index(val)*(2**(max_len - index))
+    }
+    total
+  }
+
 end
