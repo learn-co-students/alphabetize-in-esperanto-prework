668d468aeb587efadfa73521c18b69c705e48d6f
Tyler Knight <tknightx31@gmail.com>
2017-05-22 00:02:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+
+  arr.sort_by! do |word|
+    alphabet_indexes = []
+      word.each_char do |char|
+      alphabet_indexes << ESPERANTO_ALPHABET.index(char)
+    end
+    alphabet_indexes
+  end
+
+  arr
+
 end
