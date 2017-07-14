5bc3481f773824c609e221163532c20e939f8fb7
joshlacey <williamjoshualacey@gmail.com>
2017-07-14 15:44:50 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alpha_hash = Hash.new {|hash, key| hash[key] = []}
+  arr.each do |string|
+  	var = []
+  	string.split(" ").join.split(//).each do |letter|
+  		var << alphabet.split(//).index(letter)
+  	end
+  	alpha_hash[var] = string
+  end
+  array = []
+  alpha_hash.sort.each do |array_string|
+  	array << array_string[1]
+  end
+  array
 end
