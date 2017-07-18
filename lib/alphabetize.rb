44ccf3a4e07946439c77ef4c9c053190ff0ac3c8
David Squires <david.squires1@gmail.com>
2017-07-18 20:18:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  hash = {}
+  alphabet.split("").each_with_index do |letter, index|
+    hash[letter] = index
+  end
+
+
+  arr.sort_by! do |phrase|
+    i = 0
+    phrase_code = []
+    while i < 7
+      phrase_code << hash[phrase[i]]
+      i+=1
+    end
+    phrase_code
+  end
+
+  arr
 end
