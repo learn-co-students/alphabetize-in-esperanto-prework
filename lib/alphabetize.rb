de7f2b3e49a2c0bcccb746d3ca2eb4492c5101b2
cpan001 <github email address>
2017-08-01 21:50:23 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require 'pry'
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+test1 = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+test2 = ["ĝuste", "ĝis revido"]
+test3 = ["ŝĝĉĥŭĵ", "ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ"]
+
 def alphabetize(arr)
   # code here
+  arr.sort_by { |phrase|
+    new_array = []
+    count = 0
+    while count < phrase.length
+      new_array.push(ALPHABET.index(phrase[count]))
+      count += 1
+    end
+    new_array
+  }
 end
