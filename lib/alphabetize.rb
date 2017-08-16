cb92366d2c29db506286f1d3382e245de873ad2e
Al Artesona <artesonaa2@gmail.com>
2017-08-16 02:07:58 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
 def alphabetize(arr)
   # code here
+  esp_alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  regular_alphabet = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|word| word.tr(esp_alphabet, regular_alphabet)}
 end
