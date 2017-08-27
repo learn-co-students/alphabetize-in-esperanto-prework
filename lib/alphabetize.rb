eea10796a58bfeeb1b57a356b6656d97037e04d4
mattgellert <mattgellert@gmail.com>
2017-08-26 20:19:24 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,8 +5,6 @@ def alphabetize(arr)
     a.split('').each_with_index {|char, i|
       return esp.index(a[i]) <=> esp.index(b[i]) if a[i] != b[i]
     }
-
-    return esp.index(' ') <=> esp.index(b[-1])
   end
 
   arr.sort{ |a,b| custom_sort(a,b,esp)}
