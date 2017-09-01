1f375f61ff7236c2fb2d8d28fbd532c76083232f
Ayreal <aeh.herman@gmail.com>
2017-09-01 16:35:13 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.with_index.to_h
+
+  arr.sort_by do |phrase|
+    phrase.chars.map { |c| alpha[c] }
+  end
 end
