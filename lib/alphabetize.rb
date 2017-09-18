0f7fc226c321e33b4aa1c2d37758e58dedb6fbeb
Dakota Lillie <bewguy101@gmail.com>
2017-09-18 01:10:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sorted = arr.sort {|a, b|
+    counter = 0
+    if alphabet.index(a[counter]) != alphabet.index(b[counter])
+      alphabet.index(a[counter]) <=> alphabet.index(b[counter])
+    else
+      while alphabet.index(a[counter]) == alphabet.index(b[counter])
+        counter += 1
+      end
+      result = alphabet.index(a[counter]) <=> alphabet.index(b[counter])
+      counter = 0
+      result
+    end
+  }
 end
