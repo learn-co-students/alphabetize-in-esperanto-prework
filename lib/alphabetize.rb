79c2f4e43d65d5742de7dd17facfe4460b5b8a16
Nemanja Drakulovic <ndrakulovic@hotmail.com>
2017-11-10 21:35:41 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |word|
+    word.chars.map { |letter| ALPHABET.index(letter) }
+  end
 end
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+puts alphabetize(arr)
