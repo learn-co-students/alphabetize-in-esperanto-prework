b172242517f08fc4feb0be25254041db5779016e
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-19 01:01:51 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  # code here
+  map = ESPERANTO_ALPHABET.split("")
+  arr.each do |word|
+    position = map.index(word[0])
+    word.insert(0, position.to_s.rjust(2, "0"))
+  end
+  answer = arr.sort!
+  #how come the array does not reflect the permanently changed words?
+  answer.each do |word|
+    word.slice!(0..1)
+  end
 end
