9b6bb18d2c935813135b157de43b14dfb99c21a6
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-11-21 02:47:06 +0000

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |word|
+    word.each_char.map { |letter| ESPERANTO_ALPHABET.index(letter) }
+  end
 end
