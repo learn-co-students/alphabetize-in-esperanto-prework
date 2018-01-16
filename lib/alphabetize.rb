6fa898ffce7d551e4acf9f5015de7cb258f9beb4
Dwire <gjdwyer8@gmail.com>
2018-01-16 18:44:45 -0500

    Pass tests

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |a|
+    a.split("").map  {|letter| p ESPERANTO_ALPHABET.index(letter)}
+  end
 end
