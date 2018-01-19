749733b6a8722f9d50b3b7ccdbeb4bfd9acfb6a0
Caroline Lee <carolinel2610@gmail.com>
2018-01-19 04:11:41 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
 def alphabetize(arr)
+  arr.sort_by{ |phrase|
+    counter = 0
+    array = []
+    while counter < phrase.length
+      array << start_value(phrase[counter])
+      counter +=1
+    end
+    array
+  }
   # code here
+end
+
+def start_value(phrase)
+  eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  (0..27).to_a.each {|x|
+    if phrase.start_with?(eSPERANTO_ALPHABET[x])
+      return x
+    end
+  }
 end
