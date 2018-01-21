4bd46304659a175690e644794f0089cc96f1f75a
Zach Vary <zach.vary@gmail.com>
2018-01-21 00:06:27 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,27 @@
+
 def alphabetize(arr)
-  # code here
+  #reworked = ESPERANTO_ALPHABET.split("")
+  # numbers = (1..29).to_a
+  # alphabetizing_hash = {}
+  # i = 0
+  # while i < ESPERANTO_ALPHABET.length do
+  #   alphabetizing_hash[ESPERANTO_ALPHABET[i]] = numbers[i]
+  #   i += 1
+  # end
+  #derp:
+  result = arr.sort_by do |phrase|
+    phrase.chars.collect do |character|
+      ESPERANTO_ALPHABET.chars.index(character)
+    end
+  end
+    # j = 0
+    # binding.pry
+    # while !(ESPERANTO_ALPHABET.include?(phrase[j]))
+    #   j += 1
+    # end
+    #
+      # alphabetizing_hash[phrase[0]]
+  result
 end
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
