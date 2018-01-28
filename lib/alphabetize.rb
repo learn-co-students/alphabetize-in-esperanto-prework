eeab426fdab43f96820a7427354bd2f49fbf24cb
Barney Fabian <bafa@tutanota.de>
2018-01-28 20:36:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,6 @@
 def alphabetize(arr)
   esp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
   arr.sort_by! do |x|
-    x.chars.collect do |c|
-      esp.index(c)
-    end
+    x.chars.collect do |c| esp.index(c) end
   end
 end
