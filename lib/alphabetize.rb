17e9123b322f6105dfa92fe9ae061c41fae0585c
Helen Liutongco <liutongcoh@gmail.com>
2018-01-29 11:17:19 -0600

    Modify alphabetize.rb

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+
+
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  if arr[0][0] != arr[1][0]
+    arr.sort do |a,b|
+      esperanto.index(a[0]) <=> esperanto.index(b[0])
+    end
+  else
+    arr.sort
+  end
 end
