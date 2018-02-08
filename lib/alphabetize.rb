d7873b026f7bd0ad76f4e89e0161c194144cf275
Ray Blasko <mray6288@gmail.com>
2018-02-08 15:12:34 -0600

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+    arr.sort do |a, b|
+        counter = 0
+        while a[counter] == b[counter]
+            counter += 1
+        end
+        ESPERANTO_ALPHABET.index(a[counter]) <=> ESPERANTO_ALPHABET.index(b[counter])
+    end
 end
