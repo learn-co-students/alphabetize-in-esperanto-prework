5e372aa2787a708e77c884107ea8392c101e39f0
nkalkstein <nkalkstein@gmail.com>
2018-02-17 16:51:16 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
 def alphabetize(arr)
-  # code here
+  # arr.sort_by { |phrase| [phrase.downcase, phrase]
+  # }
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
