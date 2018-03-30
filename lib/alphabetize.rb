ede34643ccb0ecf368002b600ff713aff577fe51
Mike C <mikecronin92@gmail.com>
2018-03-30 05:49:46 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+
 def alphabetize(arr)
   # code here
+  esp_alpha = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
+  thing = arr.sort_by do |word|
+    word.split('').map {|ltr| esp_alpha.index(ltr)}.compact
+  end
+
+  return thing
 end
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+alphabetize(arr)
+require 'pry'
