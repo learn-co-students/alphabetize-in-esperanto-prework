caf6b74b01dfc8c347758aeb6a907384ab521514
Ryan O'Toole <otoolerj@gmail.com>
2018-03-30 21:15:55 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-# esperanto_array = ESPERANTO_ALPHABET.split("")
 
 def alphabetize(arr)
-# ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 esperanto_array = ESPERANTO_ALPHABET.split("")
   arr.sort_by do |string|
     string.split("").collect do |letter|
