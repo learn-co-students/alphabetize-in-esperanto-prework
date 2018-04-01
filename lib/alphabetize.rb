2a3955e9087e3759ecae54906462a26e111a806e
Ian Pollack <ianjpollack@gmail.com>
2018-04-01 21:28:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  espe_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  arr.sort_by do |phrase|
+    phrase.chars.map do |i|
+      espe_alpha.index(i)
+    end
+  end
 end
