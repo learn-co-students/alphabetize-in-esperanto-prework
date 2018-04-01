c577af8f67bce7fe0216c53d1cd7a70be26457e0
AustinPaley <austinpaley32@gmail.com>
2018-03-31 23:55:46 -0400

    Complete

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |string|
+    string.split("").collect do |letters|
+      ESPERANTO_ALPHABET.index(letters)
+    end
+  end
 end
