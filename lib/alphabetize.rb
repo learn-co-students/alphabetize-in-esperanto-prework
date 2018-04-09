eac1d8e554f7ddee863938820278d48502800ed6
travisdock@gmail.com <travisdock@gmail.com>
2018-04-09 19:34:18 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.chars.map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
+
+
+
+#{}"cĉ - gĝ - hĥ - jĵ - sŝ - uŭ"
+
+#x.gsub(/[ĉĝĥĵŝŭ]/, 'ĉ' => 'c', 'ĝ' => 'g', 'ĥ' => 'h', 'ĵ' => 'j', 'ŝ' => 's', 'ŭ' => 'u')
