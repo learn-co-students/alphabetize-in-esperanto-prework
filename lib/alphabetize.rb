0dd43bab3979855f651161e69226ce978f929644
jessebillard <jessebillard@mac.com>
2018-04-10 13:42:49 -0400

    Alphabetize Esperanto

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+    
+    esperanto_alphabet_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
+    esperanto_hash = esperanto_alphabet_array.map.with_index do |k, i|
+        [k, i]
+        end.to_h
+    
+    
+    arr.sort_by do |phrase|
+        phrase_array = phrase.split(//)
+        phrase_array.collect do |x|
+            esperanto_hash[x]
+        end
+    end
 end
+
+expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+alphabetize(expressions)
