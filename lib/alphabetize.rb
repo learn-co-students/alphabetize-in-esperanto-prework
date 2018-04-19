1a6317840c739e5c1affd9ad5b298de01a293f23
Ryan Ghaida <ryaboug@gmail.com>
2018-04-19 23:37:21 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
-def alphabetize(arr)
-  # code here
+Esperanto = 'abcĉdefgĝhĥijĵklmnoprsŝtuŭvz'
+
+ def alphabetize(arr)
+
+  index = 0
+
+  final_arr = arr
+
+  until final_arr != arr
+    final_arr = arr.sort_by { |word|
+      Esperanto.index(word[index])
+    }
+    index += 1
+  end
+  final_arr
 end
