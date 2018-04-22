244ee3bcfcfddd787917a761116a985f70d962a7
joshpson <jpearson846@gmail.com>
2018-04-21 21:22:59 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  #set alphabet to sort by
+  espernato = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  #sort through array
+  arr.sort_by do |item|
+    #blank array to store character positions
+    index_array = []
+    #loop through each character in item, pushing
+    #place in alphabet into array
+    item.chars.each do |character|
+      check_index = espernato.index(character)
+      index_array.push(check_index)
+    end
+    #return the array of positions
+    index_array
+  end
 end
