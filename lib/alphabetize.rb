dad3d750f85bae67448f91ac35338df7ae86ce37
JohnZBurnett <tuninator@gmail.com>
2018-04-23 14:07:06 -0400

    Wrote alphabetize method.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ASCII = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by { |string| string.tr(ESPERANTO_ALPHABET, ASCII) }
 end
