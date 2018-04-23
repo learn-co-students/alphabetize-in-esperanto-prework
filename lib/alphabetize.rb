dd0c0b2858568872652e3202d9c13e9a48d546a1
Chris Alberti <christopherjalberti@gmail.com>
2018-04-23 04:54:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,26 @@
 def alphabetize(arr)
-  # code here
+  result = {}
+  arr.each do |phrase|
+    reformatted = ""
+    phrase.each_char do |char|
+      case char
+      when "ĉ"
+        reformatted << "c"
+      when "ĝ"
+        reformatted << "g"
+      when "ĥ"
+        reformatted << "h"
+      when "ĵ"
+        reformatted << "j"
+      when "ŝ"
+        reformatted << "s"
+      when "ŭ"
+        reformatted << "u"
+      else
+        reformatted << char
+      end
+    end
+    result[phrase] = reformatted
+  end
+  result.keys.sort_by {|key| result[key]}
 end
