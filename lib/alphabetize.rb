3efdd93d61cc86ecf8fd1a40068648639238f089
Ian Canino <ian.canino@gmail.com>
2018-04-23 04:13:50 -0400

    implemented alphabetizing method

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+# Sort by the 'rank' value os determined by ESPERANTO_ALPHABET of each letter of the word
+  arr.sort_by do |word|
+    rank_value = []
+    word.chars.each do |character|
+      rank_value << ESPERANTO_ALPHABET.index(character)
+    end
+  rank_value
+  end
 end
