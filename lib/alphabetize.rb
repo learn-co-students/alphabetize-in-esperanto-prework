379be8d22d2d9ee0e57d5be166b7a8b531de5d05
jarretbryan <jarret.bryan@gmail.com>
2018-05-03 16:55:10 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_rank = esperanto.split("")
+  #break up the alphabet into an array, which by definition, ranks every
+  #letter with an index number
+
+  #binding.pry
+  arr.sort_by do |string|
+    string.split("").collect do |letters|
+      esp_rank.index(letters)
+      #iterate through the array of strings, split up the strings into their own
+      #discrete strings, rank them by position in the esp_rank array, "reassemble"
+    end
+  end
 end
