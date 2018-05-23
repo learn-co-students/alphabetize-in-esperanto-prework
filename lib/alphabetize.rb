aed20138b52d2c75ac0e17f00f2bf0f7c3625b5d
Matt Maher <mattmaher0@gmail.com>
2018-05-23 19:00:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
+Esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+Alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by {|string| string.tr(Esperanto_alphabet, Alphabet)}
 end
