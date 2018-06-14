d6dcd50dcf72426fd84f63eb56ac0029d3d7ad8b
Shumei Huang <rose1264@gmail.com>
2018-06-14 21:47:30 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,5 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by { |string| string.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
