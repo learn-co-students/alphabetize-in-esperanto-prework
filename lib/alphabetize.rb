9ed1e749c79c1abeafd3e51098386365905a05c1
Jason Pitts <juniorjr083@gmail.com>
2018-06-17 00:40:03 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
+  
+  
+  arr.sort_by do |word|
+    word.split("").map do |c|
+      ESPERANTO_ALPHABET.index(c)
+    end 
+    
+    
+  end 
 end
