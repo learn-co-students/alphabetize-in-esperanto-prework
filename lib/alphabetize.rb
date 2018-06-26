598a322205887e5e35125692f013758076800ee2
Yicheng Jin <bobjin42@gmail.com>
2018-06-26 22:27:12 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
   # code here
+  list  = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  
+  new_arr = arr.sort_by{|sentence|
+    sentence.split(" ").sort_by{|word|
+      word.split("").sort_by{|char|
+        list.index(char)
+      }
+    } 
+  }
+  
+  new_arr.sort_by{|sentence|
+    list.index(sentence[0])
+  }
 end
