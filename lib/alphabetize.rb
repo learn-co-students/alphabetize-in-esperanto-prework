200f95222781be369c5a1e91184307e4eab3ff37
Jesse McReady <jessemcready@gmail.com>
2018-07-01 02:21:26 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "abcdefghijklmnopqrstuvwxyz"
+  arr.sort_by { |word| word.tr(esperanto_alphabet, ascii) }
 end
