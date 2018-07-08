6a1f6d89130ecaccf190b861a1353add39fb0fad
derickcastillo38 <9derick@gmail.com>
2018-07-08 14:23:02 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  #binding.prys
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  split = arr.sort_by do |string|
+    string.split("").map do |word|
+      esperanto.index(word)
+    end
+  end
+
 end
