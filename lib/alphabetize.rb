d1a3d478f7651d9c38e05862d984bdd90a08bfa5
github username <github email address>
2018-07-09 17:42:17 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  sorted = arr.sort_by { |word| word.split }
+
+  sorted.sort_by { |sorted_word| esperanto.index(sorted_word[0]) }
+
 end
