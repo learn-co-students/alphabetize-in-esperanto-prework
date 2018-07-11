12dfdbb0c6f72c061c6ef321e02539ad6bebf66d
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-11 11:39:46 +0000

    Esperanto

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
 def alphabetize(arr)
-  # code here
+  alphabet_array = ESPERANTO_ALPHABET.split("")
+  new_arr = arr.sort_by{|word| alphabet_array.index word[0]}
+  length = (0..(arr[0].length-1)).to_a 
+
+  length.each do |l|
+    if arr.all? {|word| word[l] == arr[0][l]}
+      new_arr = new_arr.sort_by{|word| alphabet_array.index word[l+1]}
+    else 
+      break
+    end
+  end
+  new_arr
 end
