2b1761e358dd728e92bbae75a37c7a5323bc614f
Madeleine Tabing <madtab7@gmail.com>
2018-07-29 20:08:58 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |x|
+    x.split('').map do |letter|
+      esperanto.index(letter)
+    end
+  end
 end
