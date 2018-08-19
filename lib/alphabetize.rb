c2ae32f298431cf1b34683950e930feeed83c3d5
EthanFe <sneakcow@gmail.com>
2018-08-19 18:56:25 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  sorted_array = arr.sort do |a, b|
+    comparison_value = 0
+    index = 0
+    while comparison_value == 0 && a.length > index && b.length > index
+      comparison_value = ESPERANTO_ALPHABET.index(a[index].downcase) <=> ESPERANTO_ALPHABET.index(b[index].downcase)
+      index += 1
+    end
+    comparison_value
+  end
 end
