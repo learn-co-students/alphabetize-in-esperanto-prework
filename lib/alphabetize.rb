da2f2e3ce2933c1974b8dd2b6147d24851253cfe
Daniel Walz <walzdan@gmail.com>
2018-08-18 23:32:40 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrases|
+    phrases.split("").map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
