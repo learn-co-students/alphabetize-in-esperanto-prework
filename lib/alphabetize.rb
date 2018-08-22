84614dbcf9c167257c7c190aa0976e49445e5c88
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-22 18:05:47 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 require"pry"
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  new_array = [] 
+  new_array = []
   arr.each do |string|
     sub_array_of_characters = string.split("")
     sub_array_of_characters.each_with_index do |character, index|
@@ -10,9 +10,18 @@ def alphabetize(arr)
       end
     end   
     new_array << sub_array_of_characters
-    new_array.sort 
-# binding.pry # Everything is ay-OK. 
-  end 
+  end
+  new_array.sort.each do |sub_array|
+ binding.pry # Everything is ay-OK.
+    sub_array.each_with_index do |element, index|
+ 
+      sub_array[index] = ESPERANTO_ALPHABET[index]
+      
+ 
+    end
+    
+  end
+  
 end 
 
 
