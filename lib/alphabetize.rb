39ed985847827e82d23d09b9a2911de5d3609ea4
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-22 03:34:53 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require"pry"
 def alphabetize(arr)
-  # code here
+#  binding.pry 
+
+# arr.sort_by do |string|
+#  string.split("").collect do |letter|
+#    ESPERANTO_ALPHABET.index(letter)
+#  end
+# end
+
+# string_array.sort_by { |string| string.length }
+
+
+
+# arr.sort_by { |string| ESPERANTO_ALPHABET }
+    
 end
+
+# matrix.sort_by { |obj| obj.size }
