148ff8395c4555ae21d06fe9ffb82f70192ef656
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-22 18:08:08 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -23,31 +23,3 @@ def alphabetize(arr)
   end
   
 end 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#OG:  .sort alphabetizes a strings within an array by having pre-decided that a < b < c... x < y < z.  It, no doubt, has done likewise for the letters of ESPERANTO_ALPHABET.
-
-#So if you break a 
-
-#string_array.sort_by { |string| string.length }
-#.sort_by works by creating what you can think of as an invisible hash. When called on an array, it calculates a set of numerical keys (known as “sort keys”), and assigns each element in the array to one of those sort keys. Then, the keys are sorted, and mapped back onto the original values. The return value is the sorted array.
-
-# arr.sort_by { |string| ESPERANTO_ALPHABET }
-
-# matrix.sort_by { |obj| obj.size }
