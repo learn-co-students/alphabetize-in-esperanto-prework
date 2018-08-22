5632f438a523467f447407a6ee0f66fc468aa507
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-22 18:21:55 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -11,15 +11,18 @@ def alphabetize(arr)
     end   
     new_array << sub_array_of_characters
   end
-  new_array.sort.each do |sub_array|
- binding.pry # Everything is ay-OK.
+  
+  final_array = []
+  sorted_new_array = new_array.sort
+  sorted_new_array.each do |sub_array|
+# binding.pry # Everything is ay-OK.
     sub_array.each_with_index do |element, index|
- 
+      if element != " "
       sub_array[index] = ESPERANTO_ALPHABET[index]
-      
- 
-    end
+      end
+    end 
+    final_array = sorted_new_array
     
   end
-  
+  final_array
 end 
