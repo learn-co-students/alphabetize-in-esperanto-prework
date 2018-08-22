d05e03f3e89472b1bc138f68b2ca82037acccca0
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-22 20:19:09 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,27 +1,36 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-def alphabetize(arr)
-  new_array = []
-  arr.each do |string|
-    sub_array_of_characters = string.split("")
-    sub_array_of_characters.each_with_index do |character, index|
-      if ESPERANTO_ALPHABET.include?(character)
-        sub_array_of_characters[index] = ESPERANTO_ALPHABET.index(character)
-      end
-    end   
-    new_array << sub_array_of_characters
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
   end
-  
-  sorted_new_array = new_array.sort
-  final_array = sorted_new_array.collect do |sub_array|
-# binding.pry # Everything is ay-OK.
-    sub_array.collect do |element|
-      if element != " "
-        element = ESPERANTO_ALPHABET[element]
-      else
-        element = element
-      end 
-#     sub_array.join
-    end 
-  end 
-  final_array.collect { |sub_array| sub_array.join }
 end
+
+
+#def alphabetize(arr)
+#  new_array = []
+#  arr.each do |string|
+#    sub_array_of_characters = string.split("")
+#    sub_array_of_characters.each_with_index do |character, index|
+#      if ESPERANTO_ALPHABET.include?(character)
+#        sub_array_of_characters[index] = ESPERANTO_ALPHABET.index(character)
+#      end
+#    end   
+#    new_array << sub_array_of_characters
+#  end
+#  
+#  sorted_new_array = new_array.sort
+#  final_array = sorted_new_array.collect do |sub_array|
+## binding.pry # Everything is ay-OK.
+#    sub_array.collect do |element|
+#      if element != " "
+#        element = ESPERANTO_ALPHABET[element]
+#      else
+#        element = element
+#      end 
+##     sub_array.join
+#    end 
+#  end 
+#  final_array.collect { |sub_array| sub_array.join }
+#end
