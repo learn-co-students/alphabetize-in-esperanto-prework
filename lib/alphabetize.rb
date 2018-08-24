2257d1910404610ce48de7f7175e6ce3ec5e423e
Nicky Liu <nickyliu91@gmail.com>
2018-08-24 12:30:18 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
 def alphabetize(arr)
   # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  array2 = []
+  count = 0
+  arr.each do |string|
+    ESPERANTO_ALPHABET.each do |letter|
+      while string[0] != letter
+        count = += 1
+      else
+        array2 << [count,string]
+      end
+    end
+  end
+  puts array2
+
 end
