2c072fa295972f37ae718d350cf4c4db7010c23f
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 13:45:31 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_alphabet_array = esperanto_alphabet.chars
+  arr.sort_by do |phrase|
+    phrase.chars.map do |letter|
+      esp_alphabet_array.index(letter)
+    end
+  end
 end
