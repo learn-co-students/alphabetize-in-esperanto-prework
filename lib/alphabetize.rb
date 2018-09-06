d126e9ff3e1658e1f4351ba0503e7c6f1e613394
Frida Casas <fridakahsas@yahoo.com>
2018-09-06 02:54:14 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(phrases)
+  esperanto_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  phrases.sort_by do |phrase|
+    phrase.chars.map { |c| esperanto_alpha.index(c) }
+  end
 end
