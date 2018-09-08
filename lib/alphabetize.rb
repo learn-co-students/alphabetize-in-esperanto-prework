32c2874f7d68be2ff02f28b43bb1237dc76d8dcf
zhifeng ou <zhifengou@hotmail.com>
2018-09-08 16:21:53 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,5 @@
 def alphabetize(arr)
   alpha="abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  # using sort_by to sort words,first convert each letter to index number by split(""),importance:not split(" ") which will convert each word,then sort_by methods to sort a list of arrays return by map
   arr.sort_by {|words| words.split("").map{|letter| letter=alpha.index(letter)}}
 end
