6471c16ac82e9d68a5e563ec1ba507c006482921
srscalia <shelby.scalia@gmail.com>
2018-09-13 11:32:41 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(esp_alph, ascii)}
+  #binding.pry
 end
+
+# source: https://stackoverflow.com/questions/35778202/how-can-i-sort-an-array-of-strings-based-on-a-non-standard-alphabet
+# info on tr: https://apidock.com/ruby/String/tr
+# When a character is found, it is replaced with the character
+# that is found at the same index in the second string argument
