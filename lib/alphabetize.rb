148a2d020b9980e87c93f078967e6d0b393afe75
Asaf Davidov <adavidov@conncoll.edu>
2018-09-13 20:57:36 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_alph_arr = esp_alph.chars
+  #binding.pry
+  arr.sort_by{|string| string.chars.collect do |x| esp_alph_arr.index(x)
+    end
+  }
 end
