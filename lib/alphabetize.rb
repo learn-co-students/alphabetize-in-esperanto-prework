bdffde6194d253c1183999ad878678f8093ba5d9
Eric Klimowich <you@example.com>
2018-09-14 15:08:38 -0400

    A-B-C 1-2-3

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |element|
+    element.split("").collect do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
