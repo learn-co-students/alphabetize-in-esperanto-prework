bd914d8c247ffe2ab100aa9f0798cc49c8036980
suttonau <sutton_mjbc@icloud.com>
2018-09-16 08:56:50 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr_into_nums = {}
+ arr.each do |phrase|
+   phrase_into_num = []
+   phrase.split('').each do |letter|
+     phrase_into_num << alphabet.index(letter).to_s
+   end
+   arr_into_nums[phrase_into_num] = phrase
+ end
+  result = []
+  sorted = arr_into_nums.sort_by {|key, value| key.map {|n| n.to_i}}
+ sorted.each {|num, phrase| result << phrase }
+ result
 end
