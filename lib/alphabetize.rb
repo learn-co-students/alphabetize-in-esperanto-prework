f27e9b9367e0754489219e5ebfe98af91891ba3f
github username <github email address>
2018-09-16 13:09:35 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,5 +4,9 @@ require 'pry'
 def alphabetize(arr)
   esparento_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esparento_array = esparento_alphabet.chars
-  arr.sort_by {|string| string.chars.map {|letter| esparento_array.index(letter)}}
+  arr.sort_by do |string|
+    string.chars.map do |letter|
+      esparento_array.index(letter)
+    end
+  end 
 end
