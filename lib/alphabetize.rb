d13c5702033484c108de19c8a52a78ca66c0032d
joarnie <johnadamarnold@gmail.com>
2018-09-16 20:11:20 +0100

    done...

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+arr = ["mi amas programadon", "ruby estas mia plej ŝatata lingvo", "ni ordigu ĉi tiujn frazojn",  "laboru fervore ludu fervore", "ĉu vi ŝatas kodon"]
+
+
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
