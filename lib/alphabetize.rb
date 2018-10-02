724e20fcd3f4ad6370d16fc7819197749d008c41
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-02 16:57:00 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |phrase|
+    phrase.chars.map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end 
 end
