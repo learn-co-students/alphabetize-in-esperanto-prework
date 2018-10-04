b36496395c8080982d6564205939a9c8a1e2fa2a
Austin Luft <ausluft@gmail.com>
2018-10-04 22:58:47 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+require "pry"
+
+# arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do | sentence |
+    sentence.split("").collect do | element |
+      ESPERANTO_ALPHABET.index(element)
+    end
+  end
 end
+
+#This didn't work because it only accounted for first letter:
+
+# def alphabetize(arr)
+#   arr.sort_by do | sentence |
+#       ESPERANTO_ALPHABET.index(sentence[0])
+#     end
+# end
