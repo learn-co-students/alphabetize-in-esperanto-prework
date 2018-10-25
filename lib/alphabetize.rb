43372348b3128a98e7d062b48a40ebe6fb33b054
isaac <oseasisaac91@gmail.com>
2018-10-24 23:42:19 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+
+  esperanto_alph_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+
+  sorted = arr.sort_by do |word|
+  word.split('').map do |letter|
+    esperanto_alph_array.index(letter)
+  end
+
+  end
+sorted
 end
