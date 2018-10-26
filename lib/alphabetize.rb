11a22c7cf3cd737a3f67dcadbbe2023ba71ebc5f
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-26 14:40:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  array.sort_by do |sentence| 
+    sentence.split("").map do |character| 
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
