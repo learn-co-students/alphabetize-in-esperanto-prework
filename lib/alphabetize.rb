cf5ac1868c1e3e70e693450a1864f96c0d68fe02
Vladimir Deryuzhenko <317110@gmail.com>
2018-11-02 16:45:33 -0400

    +

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.split("").map do |character|
+      esperanto_alphabet.index(character)
+    end
+  end
 end
