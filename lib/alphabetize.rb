a6507e8f73842b8df13d106ddbdd1e52747515c5
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 10:13:26 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,41 @@
-def alphabetize(arr)
+require 'pry'
+def alphabetize(array)
   # code here
+  esp_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+#  binding.pry
+#   ["a",
+# "b",
+# "c",
+# "ĉ",
+# "d",
+# "e",
+# "f",
+# "g",
+# "ĝ",
+# "h",
+# "ĥ",
+# "i",
+# "j",
+# "ĵ",
+# "k",
+# "l",
+# "m",
+# "n",
+# "o",
+# "p",
+# "r",
+# "s",
+# "ŝ",
+# ]
+
+  solution = array.sort_by do |i|
+    #i is element level
+#    binding.pry
+    i.split('').map do |j|
+      #j is word
+#   binding.pry
+     esp_array.index(j)
+    end
+  end
+  solution
 end
