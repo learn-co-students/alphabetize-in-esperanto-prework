ab47d30d2e2748fc47cc6bb4a385bee482244077
JC Chang <jerryjcchang@gmail.com>
2018-11-18 22:42:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
 def alphabetize(arr)
-  # code here
+  finished = false
+  i = 0
+  while !finished
+    
+    if i != arr.length - 1
+      str1 = arr[i]
+      str2 = arr[i + 1]
+      if should_be_swaped?(str1, str2)
+        arr[i] = str2
+        arr[i + 1] = str1
+      end
+      i += 1
+    else
+      finished = check_arr(arr)
+      i = 0
+    end
+
+  end
+  arr
 end
