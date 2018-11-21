4aed2109952d622ab2ee55b4a3acd6c0faeedd58
Tiffany Carter <tafby88@gmail.com>
2018-11-21 01:31:22 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+
+
 def alphabetize(arr)
-  # code here
+  alaphbet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word|
+  word.split('').map do |letter|
+    alaphbet.index(letter)
+    end
+  end
 end
