109fd6ed632fa3fea80ce7e236248c73de9e22bd
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-26 16:35:42 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,10 @@
+require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-arr.sort_by {|obj| #some code here}
+esp = ESPERANTO_ALPHABET.chars
+  arr.sort_by do |obj| 
+    obj.chars.map do |letter|
+      esp.index(letter)
+    end
+  end
 end
