91db4f3a15fdd3f0a97cad1126b3fe3205966cf0
mkay0961 <mkay0961@gmail.com>
2018-12-28 14:25:37 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  arr.sort_by {
+    |word| word.chars.map{
+     |letter| "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".index(letter)
+      #binding.pry
+     }
+   }
+
 end
