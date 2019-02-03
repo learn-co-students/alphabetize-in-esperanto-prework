522c22ef569428d97e89c25e3c5b086ae86772f8
stephenyoon84 <stephenyoon84@gmail.com>
2019-02-02 20:15:09 -0500

    Alphabetize-in-esperanto - Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |x|
+    x.split('').collect do |y|
+      ESPERANTO_ALPHABET.index(y)
+    end
+  end
 end
