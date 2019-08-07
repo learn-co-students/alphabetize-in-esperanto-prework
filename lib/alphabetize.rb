a452c9bbe725199ca76124bfe8a8609bc36c474f
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:22:40 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require "pry"
+
+# ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
 def alphabetize(arr)
-  # code here
+  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  arr.sort_by do |word| 
+    word.split("").collect do |letter|
+      esperanto_arr.index(letter)
+    end 
+  end 
 end
+
+
+
+
+
+
