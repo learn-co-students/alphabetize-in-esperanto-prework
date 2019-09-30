c017dada8a34f1bd8fddb2979667cb30b9f21d95
brinda behal <brindab50@gmail.com>
2019-09-30 21:52:31 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,14 @@
 require'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  arr.sort_by do |index|
+  arr.sort_by do |element|
+    arr.split("").map
+    
  
  
   
   #binding.pry  
   end
-end
+  end
